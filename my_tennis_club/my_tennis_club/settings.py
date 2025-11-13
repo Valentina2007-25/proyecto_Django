1efaa58f7f8ee0ebdecc6bea3948d4fc8efda174
Karen valentina chaparro cardenas <Karen.chaparro71@soy.sena.edu.co>
2025-11-13 11:42:09 -0500

    creacion de datos en DB SQLite

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8zx%&*w-a0p=f1&4(w2*4o6u#=jf+d%+)a2ux0i%mpbzx$w^ji
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
